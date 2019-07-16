e25cbbf131bc2f89c109e2b2ed17e5c0b13b0e94
dominic white <domw30@gmail.com>
2019-07-16 09:45:14 +0100

    add and pass test to add two names and birthdays

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,7 +5,13 @@ describe BirthdayList do
   describe '#add' do
     it 'adds a name and a birthday to the list' do
       birthday_list = BirthdayList.new
-      expect(birthday_list.add('Dominic White', '30 January 1991')).to eq [{ name: 'Dominic White', birthday: '30 January 1991' }] 
+      expect(birthday_list.add('Dominic White', '30 January 1991')).to eq [{ name: 'Dominic White', birthday: '30 January 1991' }]
+    end
+
+    it 'adds two names and birthdays to the list' do
+      birthday_list = BirthdayList.new
+      birthday_list.add('Dominic White', '30 January 1991')
+      expect(birthday_list.add('Benjamin White', '01 August 1989')).to eq [{ name: 'Dominic White', birthday: '30 January 1991'}, { name: 'Benjamin White', birthday: '01 August 1989' }]
     end
   end
 end
